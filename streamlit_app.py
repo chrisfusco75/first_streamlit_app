05508715b1d6e333ecd977c9e2409d8100e3e81a
Chris Fusco <chrisfusco75@gmail.com>
2022-11-08 10:14:46 -0500

    Update streamlit_app.py
    update json display

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,6 @@ streamlit.dataframe(fruits_to_show)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlist.dataframe(fruityvice_normalized)
