c1213f464e01f7e384842fc4dd4fbbeffb4d3d1c
Chris Fusco <chrisfusco75@gmail.com>
2022-11-08 10:15:32 -0500

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas 
+import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit') 
@@ -16,9 +17,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlist.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
